e2830deaad5d206672b6b903b161ab2bdbbacaf8
QSDante <QSDante@live.com>
2014-08-15 22:15:41 +0900

    Add comments; A little Refactoring; Bugfix.
    添加注释；
    一点细节重构；
    Bugfix.

--- api/Msg.php
+++ api/Msg.php
@@ -1,12 +1,16 @@
 <?php
 /**
-* 
-*/
+ * @author ZongPei Huang
+ */
 class Msg{
 	protected $ip;
 	protected $port;
 	protected $sock;
 
+	/**
+	 * @param <config> Array(keys: ip, port), use the config if given
+	 * @throws Exception socket error with Error No.
+	 */
 	public function __construct($config = NULL){
 		$this->sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 		if(FALSE === $this->sock) throw new Exception(socket_strerror($errno=socket_last_error()), $errno);
@@ -17,14 +21,20 @@ class Msg{
 		$this->port = isset($config['port']) ? $config['port'] : 8889;
 	}
 
+	/**
+	 * Send message to the Messaging Server
+	 * @param <msg> Array of data be sent
+	 * @return No return
+	 * @throws Exception error message (if socket error, with Error No.)
+	 */
 	public function send($msg){
 		$msg = json_encode($msg);
 		if(FALSE === $msg) throw new Exception('Unsupported param type! $msg should be an Array.');
 
 		if(!socket_connect($this->sock, $this->ip, $this->port))
-			throw new Exception(socket_strerror($errno=socket_last_error(($this->sock)), $errno);
+			throw new Exception(socket_strerror($errno=socket_last_error($this->sock), $errno));
 		if(FALSE === socket_write($this->sock, $msg))
-			throw new Exception(socket_strerror($errno=socket_last_error(($this->sock)), $errno);
+			throw new Exception(socket_strerror($errno=socket_last_error($this->sock), $errno));
 		socket_close($this->sock);
 	}
 }
--- app/msg.js
+++ app/msg.js
@@ -1,20 +1,42 @@
+/**
+ * @author ZongPei Huang
+ */
 var qs = require("querystring"),
 	url = require("url"),
 	net = require("net");
 
 var pollings = [];
 
+/**
+ * Response jsonp
+ * @param <res> instance of http.ServerResponse
+ * @param <jsonpCb> String, jsonp callback function name
+ * @param <json> Object, the json data
+ * @return void
+ */
+function resJsonp(res, jsonpCb, json){
+	res.writeHead(200, {"Content-Type": "text/json-p"});
+	res.write(jsonpCb +"("+ JSON.stringify(json) +");");
+	res.end();
+}
+
+/**
+ * Example of http handler
+ * @param <res> instance of http.ServerResponse
+ * @param <req> instance of http.IncomingMessage
+ * @return void
+ */
 function get(res, req){
+	// push request to the polling queue
 	pollings.push({cb:qs.parse(url.parse(req.url).query).callback, res:res});
 	console.log(pollings.length, "getting");
 }
 
 function send(res, req){
 	var get = qs.parse(url.parse(req.url).query);
+	// response the polling clients
 	for(var lp; lp = pollings.shift();){
-		lp.res.writeHead(200, {"Content-Type": "application/json-p"});
-		lp.res.write(lp.cb +"("+ JSON.stringify({"msg":get.msg, "sender":get.name}) +")");
-		lp.res.end();
+		resJsonp(lp.res, lp.cb, {"msg":get.msg, "sender":get.name});
 	}
 
 	res.writeHead(200, {"Content-Type": "text/json-p"});
@@ -22,11 +44,15 @@ function send(res, req){
 	res.end();
 }
 
+/**
+ * Socket handler, forwarding message from socket client
+ * @param <dat> Object, data sent from socket client
+ * @return void
+ */
 function notifySysMsg(dat){
+	// response the polling clients
 	for(var lp; lp = pollings.shift();){
-		lp.res.writeHead(200, {"Content-Type": "application/json-p"});
-		lp.res.write(lp.cb +"("+ JSON.stringify({"msg":dat.msg, "sender":"System"}) +")");
-		lp.res.end();
+		resJsonp(lp.res, lp.cb, {"msg":dat.msg, "sender":"System"});
 	}
 }
 
